e692109e6e2b772f36090013d1729b8d60c54d13
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-22 18:34:19 +0530

    Create EmployeeBonusess.java

---
+++ Level-2 Problem/EmployeeBonusess.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EmployeeBonusess{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salary = sc.nextDouble();
+        int years = sc.nextInt();
+      
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println(bonus);
+        } 
+        
+        else {
+            System.out.println(0);
+        }
+    }
+}
