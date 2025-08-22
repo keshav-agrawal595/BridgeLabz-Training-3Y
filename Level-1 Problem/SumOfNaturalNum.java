db5cb9a034b665f6305317cad04bc0c1299d6d97
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-22 17:47:20 +0530

    Create SumOfNaturalNum.java

---
+++ Level-1 Problem/SumOfNaturalNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfNaturalNum {
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+      
+        int num = sc.nextInt();
+      
+        if (num >= 0) {
+            int sum = num * (num + 1) / 2;
+            System.out.println("The sum of " + num + " natural numbers is " + sum);
+        } else {
+            System.out.println("The number " + num + " is not a natural number");
+          
+        }
+      
+    }
+}
