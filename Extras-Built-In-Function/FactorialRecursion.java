1d75faf0aa9c42c4ebd3b46b7fb1078ff7977d7f
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 21:56:26 +0530

    Create FactorialRecursion.java

---
+++ Extras-Built-In-Function/FactorialRecursion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Q6_FactorialRecursion {
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Factorial: " + factorial(n));
+    }
+}
