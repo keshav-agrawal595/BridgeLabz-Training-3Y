fa8297414fd6f851951d2283495fc39bca6a4a97
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 14:10:04 +0530

    Add FrequencyNested class to count character frequencies

---
+++ Java-String-Practices/Level-3 Practice Problems/FrequencyNested.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class FrequencyNested {
+    public static String[] frequency(String s) {
+        char[] chars = s.toCharArray();
+        int[] freq = new int[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '0') continue;
+            freq[i] = 1;
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] == chars[j]) {
+                    freq[i]++;
+                    chars[j] = '0';
+                }
+            }
+        }
+        String[] res = new String[chars.length];
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '0') res[index++] = chars[i] + " " + freq[i];
+        }
+        return Arrays.copyOf(res, index);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[] result = frequency(text);
+        for (String r : result) System.out.println(r);
+    }
+}
