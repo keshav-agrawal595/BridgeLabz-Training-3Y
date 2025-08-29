5618cc3f65cdcc9385579ea84c6b783d004ee349
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 12:52:10 +0530

    Add StudentScorecard class for score generation and display

---
+++ Java-String-Practices/Level-2 Practice Problems/StudentScorecard.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class StudentScorecard {
+
+    public static int[][] generateScores(int n) {
+        Random rand = new Random();
+        int[][] scores = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                scores[i][j] = 40 + rand.nextInt(61);
+            }
+        }
+        return scores;
+    }
+
+    public static double[][] calculateResults(int[][] scores) {
+        int n = scores.length;
+        double[][] results = new double[n][3];
+        for (int i = 0; i < n; i++) {
+            int total = scores[i][0] + scores[i][1] + scores[i][2];
+            double average = total / 3.0;
+            double percentage = (total / 300.0) * 100.0;
+            results[i][0] = Math.round(total * 100.0) / 100.0;
+            results[i][1] = Math.round(average * 100.0) / 100.0;
+            results[i][2] = Math.round(percentage * 100.0) / 100.0;
+        }
+        return results;
+    }
+
+    public static String[] calculateGrades(double[][] results) {
+        int n = results.length;
+        String[] grades = new String[n];
+        for (int i = 0; i < n; i++) {
+            double perc = results[i][2];
+            if (perc >= 80) grades[i] = "A";
+            else if (perc >= 70) grades[i] = "B";
+            else if (perc >= 60) grades[i] = "C";
+            else if (perc >= 50) grades[i] = "D";
+            else if (perc >= 40) grades[i] = "E";
+            else grades[i] = "R";
+        }
+        return grades;
+    }
+
+    public static void displayScorecard(int[][] scores, double[][] results, String[] grades) {
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.printf("%-8s %-8s %-8s %-8s %-8s %-10s %-12s %-8s\n", 
+                          "StudID", "Physics", "Chem", "Math", "Total", "Average", "Percentage", "Grade");
+        System.out.println("--------------------------------------------------------------------------------------------");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.printf("%-8d %-8d %-8d %-8d %-8.0f %-10.2f %-12.2f %-8s\n",
+                    (i+1), scores[i][0], scores[i][1], scores[i][2],
+                    results[i][0], results[i][1], results[i][2], grades[i]);
+        }
+        System.out.println("--------------------------------------------------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int[][] scores = generateScores(n);
+        double[][] results = calculateResults(scores);
+        String[] grades = calculateGrades(results);
+        displayScorecard(scores, results, grades);
+    }
+}
