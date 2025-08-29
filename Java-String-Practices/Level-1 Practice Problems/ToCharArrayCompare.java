f7265d5aecc175833a728164f30557634dec314a
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 12:32:09 +0530

    Add ToCharArrayCompare class for string comparison

---
+++ Java-String-Practices/Level-1 Practice Problems/ToCharArrayCompare.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ToCharArrayCompare {
+    public static char[] toChars(String s) {
+        char[] arr = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) arr[i] = s.charAt(i);
+        return arr;
+    }
+    public static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) if (a[i] != b[i]) return false;
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr1 = toChars(s);
+        char[] arr2 = s.toCharArray();
+        System.out.println(compareArrays(arr1, arr2));
+    }
+}
