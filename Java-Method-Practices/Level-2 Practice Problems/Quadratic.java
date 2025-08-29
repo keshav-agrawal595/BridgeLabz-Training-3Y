633b2930a2db90a0320a61f091436f24375896bc
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 16:37:08 +0530

    Implement quadratic equation root finder

---
+++ Java-Method-Practices/Level-2 Practice Problems/Quadratic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Quadratic {
+    public static double[] findRoots(double a, double b, double c) {
+        double delta = b*b - 4*a*c;
+        if (delta < 0) return new double[]{};
+        if (delta == 0) return new double[]{-b / (2*a)};
+        double root1 = (-b + Math.sqrt(delta)) / (2*a);
+        double root2 = (-b - Math.sqrt(delta)) / (2*a);
+        return new double[]{root1, root2};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double[] roots = findRoots(a, b, c);
+        if (roots.length == 0) System.out.println("No Real Roots");
+        else {
+            for (double r : roots) System.out.println("Root: " + r);
+        }
+    }
+}
