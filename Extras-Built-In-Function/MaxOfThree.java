0c5abe8a343a16dc6ee3ed852af67883f71e2eee
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 21:54:43 +0530

    Create MaxOfThree.java

---
+++ Extras-Built-In-Function/MaxOfThree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Q2_MaxOfThree {
+    static int maxOfThree(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Maximum: " + maxOfThree(a, b, c));
+    }
+}
