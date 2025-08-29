864eb12c520969357e4d07d37d9db1ea0dfc766a
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-29 13:07:22 +0530

    Implement first non-repeating character finder

---
+++ Java-String-Practices/Level-3 Practice Problems/FirstNonRepeating.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FirstNonRepeating {
+    public static char firstNonRepeatingChar(String s) {
+        int[] freq = new int[256];
+        for (int i = 0; i < s.length(); i++) freq[s.charAt(i)]++;
+        for (int i = 0; i < s.length(); i++) if (freq[s.charAt(i)] == 1) return s.charAt(i);
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        char c = firstNonRepeatingChar(text);
+        if (c == '\0') System.out.println("No non-repeating character");
+        else System.out.println(c);
+    }
+}
